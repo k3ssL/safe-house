9457386cdf734518f587b5fd8860613a24d912e9
Aleksey <viqwer3@yandex.ru>
2026-05-18 01:17:18 +0500

    remove change avatar button

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -48,12 +48,12 @@ const Profile = () => {
                 <div className={styles.header}>
                     <div className={styles.avatar_container}>
                         <img src={user?.avatar} alt={user?.name} className={styles.avatar} />
-                        <button className={styles.change_avatar_btn}>
-                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"/>
-                                <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"/>
-                            </svg>
-                        </button>
+                        {/*<button className={styles.change_avatar_btn}>*/}
+                        {/*    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">*/}
+                        {/*        <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"/>*/}
+                        {/*        <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"/>*/}
+                        {/*    </svg>*/}
+                        {/*</button>*/}
                     </div>
                     <div className={styles.user_info}>
                         {isEditing ? (
